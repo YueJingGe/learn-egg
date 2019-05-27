b5453bdca2e54b53c716cfc23f44e0b88c5861a8
wb-yjg301072 <wb-yjg301072@alibaba-inc.com>
2019-05-27 17:48:38 +0800

    初步配置egg项目

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # learn-egg
 egg初步
+
+## 启动
+```
+$ npm run dev
+$ open localhost:7001
+```
---
+++ config/config.default.js
@@ -0,0 +1,20 @@
+module.exports = appInfo => {
+  const config = {};
+
+  config.keys = appInfo.name + "_1493400288522_208";
+  config.view = {
+    defaultViewEngine: "nunjucks",
+    mapping: {
+      ".tpl": "nunjucks"
+    }
+  };
+  config.news = {
+    pageSize: 5,
+    serverUrl: "https://hacker-news.firebaseio.com/v0"
+  };
+  config.middleware = ["robot"];
+  config.robot = {
+    ua: [/curl/i, /Baiduspider/i]
+  };
+  return config;
+};
